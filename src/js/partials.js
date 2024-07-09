0bc7bdc47adb65903e81fc8501b8c22760ba635d
milamelo4 <mel19009@byui.edu>
2024-07-09 15:08:24 -0600

    remove log in all together

--- src/js/partials.js
+++ src/js/partials.js
@@ -5,47 +5,47 @@ import {
   getLocalStorage,
 } from "./utils.mjs";
 
-// export async function renderWithTemplate(
-//   templateFn,
-//   parentElement,
-//   data,
-//   callback,
-//   position = "afterbegin",
-//   clear = true
-// ) {
-//   if (clear) {
-//     parentElement.innerHTML = "";
-//   }
-//   const htmlString = await templateFn(data);
-//   parentElement.insertAdjacentHTML(position, htmlString);
-//   if (callback) {
-//     callback(data);
-//   }
-// }
+// // export async function renderWithTemplate(
+// //   templateFn,
+// //   parentElement,
+// //   data,
+// //   callback,
+// //   position = "afterbegin",
+// //   clear = true
+// // ) {
+// //   if (clear) {
+// //     parentElement.innerHTML = "";
+// //   }
+// //   const htmlString = await templateFn(data);
+// //   parentElement.insertAdjacentHTML(position, htmlString);
+// //   if (callback) {
+// //     callback(data);
+// //   }
+// // }
 
-// function loadTemplate(path) {
-//   // wait what?  we are returning a new function?
-//   // this is called currying and can be very helpful.
-//   return async function () {
-//     const res = await fetch(path);
-//     if (res.ok) {
-//       const html = await res.text();
-//       return html;
-//     }
-//   };
-// }
+// // function loadTemplate(path) {
+// //   // wait what?  we are returning a new function?
+// //   // this is called currying and can be very helpful.
+// //   return async function () {
+// //     const res = await fetch(path);
+// //     if (res.ok) {
+// //       const html = await res.text();
+// //       return html;
+// //     }
+// //   };
+// // }
 
-// export async function loadHeaderFooter() {
-//   const headerTemplateFn = loadTemplate("/wdd330-personal-project/partials/header.html");
-//   //const footerTemplateFn = loadTemplate("/partials/footer.html");
-//   const headerEl = document.querySelector("#main-header");
-//   //const footerEl = document.querySelector("#main-footer");
-//   await renderWithTemplate(headerTemplateFn, headerEl);
-//     console.log("Header loaded and rendered.");
-//   //renderWithTemplate(footerTemplateFn, footerEl);
-//   toggleNav();
-//   welcomeMsg();
-// }
+// // export async function loadHeaderFooter() {
+// //   const headerTemplateFn = loadTemplate("/wdd330-personal-project/partials/header.html");
+// //   //const footerTemplateFn = loadTemplate("/partials/footer.html");
+// //   const headerEl = document.querySelector("#main-header");
+// //   //const footerEl = document.querySelector("#main-footer");
+// //   await renderWithTemplate(headerTemplateFn, headerEl);
+// //     console.log("Header loaded and rendered.");
+// //   //renderWithTemplate(footerTemplateFn, footerEl);
+// //   toggleNav();
+// //   welcomeMsg();
+// // }
 
 export function toggleNav() {
   let menuIcon = qs("#menu-icon");
@@ -61,43 +61,43 @@ export function toggleNav() {
   }
 }
 
- function welcomeMsg() {
-   const userN = qs("#displayUserName");
-   if (!userN) {
-     console.error("User name display element not found");
-     return;
-   }
+//  function welcomeMsg() {
+//    const userN = qs("#displayUserName");
+//    if (!userN) {
+//      console.error("User name display element not found");
+//      return;
+//    }
 
-   let welcomeMsgDisplay = false;
-   if (welcomeMsgDisplay) return;
+//    let welcomeMsgDisplay = false;
+//    if (welcomeMsgDisplay) return;
 
-   let nameValue = getLocalStorage("userName");
-   if (!nameValue) {
-     nameValue = "Guest";
-   }
+//    let nameValue = getLocalStorage("userName");
+//    if (!nameValue) {
+//      nameValue = "Guest";
+//    }
 
-   let charInx = 0;
-   const welcomeMessage = `Welcome ${capitalize(nameValue)}🤍`;
+//    let charInx = 0;
+//    const welcomeMessage = `Welcome ${capitalize(nameValue)}🤍`;
 
-   function type() {
-     if (charInx < welcomeMessage.length) {
-       userN.textContent += welcomeMessage.charAt(charInx);
-       charInx++;
-       setTimeout(type, 200);
-     } else {
-       welcomeMsgDisplay = true;
-     }
-   }
+//    function type() {
+//      if (charInx < welcomeMessage.length) {
+//        userN.textContent += welcomeMessage.charAt(charInx);
+//        charInx++;
+//        setTimeout(type, 200);
+//      } else {
+//        welcomeMsgDisplay = true;
+//      }
+//    }
 
-   type();
- }
-document.addEventListener("DOMContentLoaded", () => {
-  toggleNav();
-  // Only call welcomeMsg if the #displayUserName element exists
-  if (document.querySelector("#displayUserName")) {
-    welcomeMsg();
-  }
-});
+//    type();
+//  }
+// document.addEventListener("DOMContentLoaded", () => {
+//   toggleNav();
+//   // Only call welcomeMsg if the #displayUserName element exists
+//   if (document.querySelector("#displayUserName")) {
+//     welcomeMsg();
+//   }
+// });
 
 
 
